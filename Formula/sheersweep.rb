91fee74ca938528c66a1e9ca6d8277ff506f41fc
cverorg <292680828+cverorg@users.noreply.github.com>
2026-07-22 22:48:56 +0900

    sheersweep 0.10.2

--- Formula/sheersweep.rb
+++ Formula/sheersweep.rb
@@ -1,8 +1,8 @@
 class Sheersweep < Formula
   desc "Mac cleaner you can read: dry-run-first, never touches your real files"
   homepage "https://github.com/CVERInc/sheersweep"
-  url "https://github.com/CVERInc/sheersweep/archive/refs/tags/v0.10.1.tar.gz"
-  sha256 "0f444ac16a12404d14c1f48bd4c2f926cb0bc9145db38fd4f3df78a2d40b258c"
+  url "https://github.com/CVERInc/sheersweep/archive/refs/tags/v0.10.2.tar.gz"
+  sha256 "2d48841a3f438339cc930f20260af56d0e101314941ca02635b95b5a3dd2c3f8"
   license "MIT"
 
   def install
